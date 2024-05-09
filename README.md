9b15738fef3c87ecee92ff5d44feee43817e736f
Firstero <firstbear0@protonmail.com>
2024-05-09 16:03:50 +0800

    FeatureAdd: finished homework https://u.geekbang.org/lesson/610?article=768417

--- README.md
+++ README.md
@@ -1,35 +1,92 @@
-# Geektime Rust 语言训练营
+# Geektime Rust 训练营
 
-## Rcli 开发
+## rcli: 基于Rust实现的命令行工具
 
-### V1-7 rcli csv 添加 csv 转换
+### 作业一
 
-    - ```rcli csv --input in.csv --output out.json --format json```
-    - ```rcli csv --input in.csv --output out.yaml --format yaml```
-    - ```rcli csv --header --delimiter , --input in.csv --output out.yaml --format yaml```
+#### 作业要求
 
-### V1-8 rcli genpass
+阅读 chacha20poly1305 文档，了解其使用方法并构建 CLI 对输入文本进行加密 / 解密 CLI接口示例：
 
-    - ```rcli genpass -l 32 --no-lower --no-lower --no-symbol --no-number```
+- rcli text encrypt -key"xxx"> 加密并输出 base64
+- rcli text decrypt -key"XXX" >base64 > binary> 解密文本
 
-### V1-9 rcli base64
+#### 实现说明
+具体实现，参考 cli/text.rs，运行命令如下
+1. 使用chacha20poly1305 crate 进行包装
+2. 增加 nonce 参数，用于每次的使用, 参考 chacha20poly1305
+- ```rcli text encrypt --key 'keyfile/or stdin' --input textfile --nonce noncefile```
+- ```rcli text decrypt --key 'keyfile/or stdin' --input textfile --nonce noncefile```
 
-    - ```rcli base64 encode --format nopadding/standard/urlsafe --input textfile```
-    - ```rcli base64 decode --format nopadding/standard/urlsafe --input textfile```
+### 作业二
 
-### V1-10/11 rcli text 加密解密
+#### 作业要求
 
-    - ```rcli text sign --format blake3 --key keyfile --input textfile```
-    - ```rcli text verify --format blake3 --key keyfile --input textfile --sig signature```
-    - ```rcli text generate --format blake3 --output keyfile```
-    - ```rcli text encrypt --key keyfile --input textfile --nonce noncefile```
-    - ```rcli text decrypt --key keyfile --input textfile --nonce noncefile```
+json web token(jwt) 在用户验证领域经常被用到。请构建一个 CLI 来为给定 sub/aud/exp/… 生成一个 jwt。要求生成的 jwt 可以通过 jwt.io 的验证。
+CLI接口示例:
+- rcli jwt sign --sub acme --aud device1 --exp 14d
+- rcli jwt verify -t
 
-### V1-12/13 rcli http serve(default dir is current dir, default port is 8080)
+#### 实现说明
 
-    - ```rcli http serve```
-    - ```rcli http serve --dir /tmp --port 8080```
+1. 使用 jsonwebtoken crate 来实现 sign/decode
+2. 直接使用 serde_json 来作为 Claims 结构, 参考 jsonwebtoken Validation 结构体, 支持所有标准规范可选字段的定制
 
-### V1-14 重构 CLI
+#### 使用指南
+- rcli jwt sign
+```
+Usage: rcli jwt sign [OPTIONS]
+Options:
+      --alg <ALG>  claims algorithm [default: HS384]
+      --key <KEY>  sign key file path, or '-' for stdin [default: -]
+      --sub <SUB>  subject
+      --aud <AUD>  audience
+      --iss <ISS>  jwt issuer
+      --exp <EXP>  jwt expiration time [default: 1d]
+      --nbf <NBF>  jwt nbf time [default: 1d]
+      --iat        generate jwt iat or not
+  -h, --help       Print help
+```
+- rcli jwt verify
+```
+Usage: rcli jwt verify [OPTIONS] --token <TOKEN>
+Options:
+  -t, --token <TOKEN>                      jwt token
+      --key <KEY>                          key file path, or '-' for stdin [default: -]
+      --required-claims <REQUIRED_CLAIMS>  required claims
+      --aud <AUD>                          audiences
+      --iss <ISS>                          issuers
+      --sub <SUB>                          jwt subject
+      --validate-exp <VALIDATE_EXP>        validate expiration time [possible values: true, false]
+      --validate-nbf <VALIDATE_NBF>        validate nbf time [possible values: true, false]
+      --validate-aud <VALIDATE_AUD>        validate audience [possible values: true, false]
+      --silent                             show token claims, if verified successfully
+      --show-self                          show self options
+  -h, --help                               Print help
+```
 
-### V1-15 作业
+### 作业三
+#### 作业要求
+
+给课程里的 HTTP 文件服务器添加对 directory index 的支持。
+
+#### 实现说明
+
+1. Service 实现: 使用 CustomServiceDir 包装原始 ServeDir
+    - 使用 axum handler 来实现自定义的 CustomServiceDir service
+    - handler 实现中基于 tower::ServiceExt 的 oneshot 来包装,实现自定义代理
+    - 由于 [ServeDir in nested route causes invalid redirects #1731]( https://github.com/tokio-rs/axum/issues/1731 ), 手动处理了 StatusCode::TEMPORARY_REDIRECT
+2. 目录索引生成实现: 使用 tokio::fs 实现目录的一级遍历
+3. 前端渲染: 使用 askama 模板引擎来实现目录页面及错误页面渲染，参考 https://github.com/ttys3/static-server
+
+#### 使用指南
+- rcli http
+```Usage: rcli http <COMMAND>
+
+Commands:
+  serve  Serve a directory via HTTP
+  help   Print this message or the help of the given subcommand(s)
+
+Options:
+  -h, --help  Print help
+```
--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -1,64 +1,306 @@
-use std::{net::SocketAddr, path::PathBuf, sync::Arc};
+use std::{ffi::OsStr, net::SocketAddr, path::PathBuf, time::SystemTime};
 
 use anyhow::Result;
+use askama::Template;
 use axum::{
-    extract::{Path, State},
-    http::StatusCode,
+    body::Body,
+    extract,
+    http::{header, HeaderValue, Request, Response, StatusCode},
+    response::{Html, IntoResponse},
     routing::get,
     Router,
 };
-use tokio::fs;
-use tower_http::services::{ServeDir, ServeFile};
-use tracing::info;
+use base64::Engine;
+use tokio::{fs, io};
+use tower::util::ServiceExt;
+use tower_http::{normalize_path::NormalizePath, services::ServeDir};
 
-#[derive(Debug)]
-struct HttpServeState {
-    path: PathBuf,
-}
-
-pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
+pub async fn process_http_serve(root_dir: PathBuf, port: u16) -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    info!("Serving {:?} on addr {}", path, addr);
-    let dir_service =
-        ServeDir::new(path.clone()).not_found_service(ServeFile::new("assets/not_found.html"));
-    let state = HttpServeState { path };
+    tracing::info!(
+        "Rcli Static File Serving directory {:?} on addr {}",
+        root_dir,
+        addr
+    );
+    let serve_base_dir = root_dir.to_string_lossy().to_string();
+    // let state = HttpServeState { root_dir };
+    let listener = tokio::net::TcpListener::bind(addr).await?;
 
     let app = Router::new()
-        .route("/*path", get(file_handler))
-        .with_state(Arc::new(state))
-        .nest_service("/tower", dir_service);
-    let listener = tokio::net::TcpListener::bind(addr).await?;
-    axum::serve(listener, app).await?;
+        .nest_service("/", get(move |req| custom_serve_dir(req, serve_base_dir)))
+        .route("/favicon.ico", get(favicon))
+        .route("/health", get(health_check));
+
+    let app = NormalizePath::trim_trailing_slash(app);
+    axum::serve(
+        listener,
+        axum::ServiceExt::<extract::Request>::into_make_service(app),
+    )
+    .await?;
     Ok(())
 }
 
-async fn file_handler(
-    State(state): State<Arc<HttpServeState>>,
-    Path(path): Path<String>,
-) -> (StatusCode, String) {
-    //TODO: improve file_handler to handle more file types
-    let path = state.path.join(path);
-    if path.exists() {
-        match fs::read_to_string(path).await {
-            Ok(content) => (StatusCode::OK, content),
-            Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
-        }
-    } else {
-        (StatusCode::NOT_FOUND, "Not Found".to_string())
+/// health check for K8s liveness and readiness probe
+async fn health_check() -> impl IntoResponse {
+    "ok"
+}
+
+async fn favicon() -> impl IntoResponse {
+    // one pixel favicon generated from https://png-pixel.com/
+    let one_pixel_favicon = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPk+89QDwADvgGOSHzRgAAAAABJRU5ErkJggg==";
+    let pixel_favicon = base64::prelude::BASE64_STANDARD
+        .decode(one_pixel_favicon)
+        .unwrap();
+    (
+        [(header::CONTENT_TYPE, HeaderValue::from_static("image/png"))],
+        pixel_favicon,
+    )
+}
+
+/// Serve a directory, Because the ServeDir service does not support listing directories, we need to implement it ourselves.
+/// If the path is a directory, list its contents.
+/// If the path is a file, use ServiDir to serve it.
+/// Because ServeDir in nested route causes invalid redirects #1731 https://github.com/tokio-rs/axum/issues/1731, handle StatusCode::TEMPORARY_REDIRECT manually here to avoid the problem.
+async fn custom_serve_dir(
+    req: Request<Body>,
+    serve_base_dir: String,
+) -> Result<impl IntoResponse, (StatusCode, String)> {
+    let mut path = req.uri().path().trim_start_matches('/').to_owned();
+    if path.is_empty() {
+        path = ".".to_string();
+    }
+    let full_path = PathBuf::from(&serve_base_dir).join(&path);
+    tracing::debug!(
+        "custom_serve_dir get req_path is {:?} and serve path={:?}",
+        path,
+        full_path
+    );
+
+    let service = ServeDir::new(&serve_base_dir);
+    let result = service.oneshot(req).await;
+    match result {
+        Ok(res) => match res.status() {
+            StatusCode::NOT_FOUND | StatusCode::TEMPORARY_REDIRECT
+                if PathBuf::from(&full_path).is_dir() =>
+            {
+                let rs = visit_dir_one_level(&full_path, &serve_base_dir).await;
+                match rs {
+                    Ok(files) => Ok(DirListTemplate {
+                        lister: DirLister { files },
+                        cur_path: path.to_string(),
+                    }
+                    .into_response()),
+                    Err(e) => Ok(ErrorTemplate {
+                        err: ResponseError::InternalError(e.to_string()),
+                        cur_path: path.to_string(),
+                        message: e.to_string(),
+                    }
+                    .into_response()),
+                }
+            }
+            StatusCode::NOT_FOUND => Ok(ErrorTemplate {
+                err: ResponseError::FileNotFound("File Not Found".to_string()),
+                cur_path: path.to_string(),
+                message: "File Not Found".to_string(),
+            }
+            .into_response()),
+            StatusCode::BAD_REQUEST => Ok(ErrorTemplate {
+                err: ResponseError::BadRequest("Bad Request".to_string()),
+                cur_path: path.to_string(),
+                message: "Bad Request".to_string(),
+            }
+            .into_response()),
+            StatusCode::INTERNAL_SERVER_ERROR => Ok(ErrorTemplate {
+                err: ResponseError::BadRequest("Internal Server Error".to_string()),
+                cur_path: path.to_string(),
+                message: "Internal Server Error".to_string(),
+            }
+            .into_response()),
+            _ => Ok(res.into_response()),
+        },
+        Err(err) => Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", err),
+        )),
     }
 }
 
+async fn visit_dir_one_level(dir_path: &PathBuf, prefix: &str) -> io::Result<Vec<FileInfo>> {
+    let mut dir = fs::read_dir(dir_path).await?;
+    let mut files: Vec<FileInfo> = Vec::new();
+
+    while let Some(child) = dir.next_entry().await? {
+        let the_path = child.path().to_string_lossy().to_string();
+        let the_uri_path: String;
+        if !prefix.is_empty() && !the_path.starts_with(prefix) {
+            tracing::error!("visit_dir_one_level skip invalid path={}", the_path);
+            continue;
+        } else if prefix != "/" {
+            the_uri_path = the_path.strip_prefix(prefix).unwrap().to_string();
+        } else {
+            the_uri_path = the_path;
+        }
+        files.push(FileInfo {
+            name: child.file_name().to_string_lossy().to_string(),
+            ext: PathBuf::from(child.file_name())
+                .extension()
+                .and_then(OsStr::to_str)
+                .unwrap_or_default()
+                .to_string(),
+            mime_type: mime_guess::from_path(child.path())
+                .first_or_octet_stream()
+                .type_()
+                .to_string(),
+            path_uri: the_uri_path,
+            is_file: child.file_type().await?.is_file(),
+            last_modified: child
+                .metadata()
+                .await?
+                .modified()?
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs() as i64,
+        });
+    }
+
+    Ok(files)
+}
+
+/// Custom filters for askama template
+mod filters {
+    pub(crate) fn datetime(ts: &i64) -> ::askama::Result<String> {
+        if let Ok(format) =
+            time::format_description::parse("[year]-[month]-[day] [hour]:[minute]:[second] UTC")
+        {
+            return Ok(time::OffsetDateTime::from_unix_timestamp(*ts)
+                .unwrap()
+                .format(&format)
+                .unwrap());
+        }
+        Err(askama::Error::Fmt(std::fmt::Error))
+    }
+}
+
+const FAIL_REASON_HEADER_NAME: &str = "Rcli-Http-Serve-Fail-Reason";
+
+pub(crate) enum ResponseError {
+    BadRequest(String),
+    FileNotFound(String),
+    InternalError(String),
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct DirListTemplate {
+    lister: DirLister,
+    cur_path: String,
+}
+
+struct FileInfo {
+    name: String,
+    ext: String,
+    mime_type: String,
+    path_uri: String,
+    is_file: bool,
+    last_modified: i64,
+}
+
+struct DirLister {
+    files: Vec<FileInfo>,
+}
+
+#[derive(Template)]
+#[template(path = "error.html")]
+struct ErrorTemplate {
+    err: ResponseError,
+    cur_path: String,
+    message: String,
+}
+
+impl IntoResponse for ErrorTemplate {
+    fn into_response(self) -> Response<Body> {
+        let t = self;
+        match t.render() {
+            Ok(html) => {
+                let mut resp = Html(html).into_response();
+                match t.err {
+                    ResponseError::FileNotFound(reason) => {
+                        *resp.status_mut() = StatusCode::NOT_FOUND;
+                        resp.headers_mut()
+                            .insert(FAIL_REASON_HEADER_NAME, reason.parse().unwrap());
+                    }
+                    ResponseError::BadRequest(reason) => {
+                        *resp.status_mut() = StatusCode::BAD_REQUEST;
+                        resp.headers_mut()
+                            .insert(FAIL_REASON_HEADER_NAME, reason.parse().unwrap());
+                    }
+                    ResponseError::InternalError(reason) => {
+                        *resp.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+                        resp.headers_mut()
+                            .insert(FAIL_REASON_HEADER_NAME, reason.parse().unwrap());
+                    }
+                }
+                resp
+            }
+            Err(err) => {
+                tracing::error!("template render failed, err={}", err);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Failed to render template. Error: {}", err),
+                )
+                    .into_response()
+            }
+        }
+    }
+}
+
+impl IntoResponse for DirListTemplate {
+    fn into_response(self) -> Response<Body> {
+        let t = self;
+        match t.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => {
+                tracing::error!("template render failed, err={}", err);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Failed to render template. Error: {}", err),
+                )
+                    .into_response()
+            }
+        }
+    }
+}
+
+// 添加单元测试，测试visit_dir_one_level函数
 #[cfg(test)]
 mod tests {
+
     use super::*;
+    use std::fs;
 
     #[tokio::test]
-    async fn test_file_handler() {
-        let state = Arc::new(HttpServeState {
-            path: PathBuf::from("."),
-        });
-        let (status, content) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
-        assert_eq!(status, StatusCode::OK);
-        assert!(content.trim().starts_with("[package]"));
+    async fn test_visit_dir_one_level() {
+        let dir = tempfile::tempdir().unwrap();
+        let dir_path = dir.path();
+        // create a file
+        fs::File::create(dir_path.join("a.txt")).unwrap();
+        // create a sub directory
+        fs::create_dir(dir_path.join("b_dir")).unwrap();
+        let mut files = visit_dir_one_level(&dir_path.to_path_buf(), "")
+            .await
+            .unwrap();
+        files.sort_by(|a, b| a.name.cmp(&b.name));
+        assert_eq!(files.len(), 2);
+        assert_eq!(files[0].name, "a.txt");
+        assert_eq!(files[0].ext, "txt");
+        assert_eq!(files[0].mime_type, "text");
+        assert!(files[0].is_file);
+        assert!(files[0].path_uri.ends_with("a.txt"));
+
+        assert_eq!(files[1].name, "b_dir");
+        assert_eq!(files[1].ext, "");
+        assert_eq!(files[1].mime_type, "application");
+        assert!(!files[1].is_file);
+        assert!(files[1].path_uri.ends_with("b_dir"));
     }
 }
